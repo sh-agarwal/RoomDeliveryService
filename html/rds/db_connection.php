ab81830a83e73412c60a24858d60005a19c47186
Mani Mannampalli <mani.mannampalli@gmail.com>
2019-05-01 23:21:20 +0530

    minor changes

--- html/rds/db_connection.php
+++ html/rds/db_connection.php
@@ -1,16 +1,9 @@
 <?php
-    if(!isset($_SERVER['HTTP_REFERER'])){
-        header( 'HTTP/1.0 403 Forbidden', TRUE, 403 );
-        header('location:../../error/error.html');
-        exit;
-    }
-?>
-<?php
 function OpenCon()
  {
  $dbhost = "localhost";
  $dbuser = "root";
- $dbpass = "Password#123";
+ $dbpass = "password#123";
  $db = "db";
  $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
 
